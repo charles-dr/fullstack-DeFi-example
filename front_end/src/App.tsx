418eb743ef4d991adea95e066392fffed0033e36
Bi Wu <alerk_star@outlook.com>
2022-03-22 21:25:24 +0800

    front: add connect button;

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Mainnet,
+  DAppProvider,
+  useEtherBalance,
+  useEthers,
+  Config,
+  ChainId,
+} from '@usedapp/core'
+import { Header } from "./components/Header";
+
+const config: Config = {
+  supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337]
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DAppProvider config={config}>
+      <Header />
+      <div className="App">
+        <h3>Hi</h3>
+      </div>
+    </DAppProvider>
   );
 }
 
